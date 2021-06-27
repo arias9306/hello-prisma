3e3bec0670b2678403a7a17672b537548dfa760c
Andrés Arias <arias9306@gmail.com>
2021-06-27 17:25:36 -0500

    adding user and post services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService, UserService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
